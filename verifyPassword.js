c531979ccfa90be3fd59639c29913badbfdcd490
giofile <monsanto.g@chello.nl>
2021-09-01 13:23:18 +0200

    test

---
+++ verifyPassword.js
@@ -0,0 +1,27 @@
+const verifyPasswordLenghth = function(passwordLength){
+    if (passwordLength >== 9 characters){
+        passwordLength ===
+    }
+
+}
+
+
+const verifyPasswordNull = function(passwordNull){
+    if (passwordNull is not Null){
+
+
+    }
+
+}
+
+const verifyPasswordToUpper = function(PasswordToUpper) {
+
+}
+
+const verifyPasswordToLower = function(PasswordToLower){
+
+}
+
+const verifyPasswordNumber = function(PasswordNumber){
+    if ( )
+}
