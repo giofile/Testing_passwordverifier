f9789662da05b247c1e06f4319550e97b2393e68
giofile <monsanto.g@chello.nl>
2021-09-01 14:02:09 +0200

    testing

--- verifyPassword.js
+++ verifyPassword.js
@@ -1,27 +1,43 @@
-const verifyPasswordLenghth = function(passwordLength){
-    if (passwordLength >== 9 characters){
-        passwordLength ===
-    }
+// Utility functions
+const isNotNull = str => str !== null;
 
-}
+const hasRightLength = str => isNotNull(str) && str.length <= 8;
 
+const hasUpperCaseCharacter = str =>
+    isNotNull(str) && str.toLowerCase() !== str;
 
-const verifyPasswordNull = function(passwordNull){
-    if (passwordNull is not Null){
+const hasLowerCaseCharacter = str =>
+    isNotNull(str) && str.toUpperCase() !== str;
 
+const hasDigit = str => /\d/.test(str);
 
-    }
+const minimumConditionsReached = conditions => {
+    // conditions is an array of booleans
+    trueConditions = conditions.filter(bool => bool);
+    return trueConditions.length >= 3;
+};
 
-}
+// "Outer" function
+const verifyPassword = password => {
+    const conditions = [
+        isNotNull(password),
+        hasRightLength(password),
+        hasUpperCaseCharacter(password),
+        hasLowerCaseCharacter(password),
+        hasDigit(password)
+    ];
+    const result =
+        minimumConditionsReached(conditions) && hasLowerCaseCharacter(password);
 
-const verifyPasswordToUpper = function(PasswordToUpper) {
+    return result;
+};
 
-}
-
-const verifyPasswordToLower = function(PasswordToLower){
-
-}
-
-const verifyPasswordNumber = function(PasswordNumber){
-    if ( )
-}
+module.exports = {
+    verifyPassword,
+    hasRightLength,
+    isNotNull,
+    hasUpperCaseCharacter,
+    hasLowerCaseCharacter,
+    hasDigit,
+    minimumConditionsReached
+};
